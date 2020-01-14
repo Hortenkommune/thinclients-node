31e5e238ea4be3964a1bdfeee362ce01e51198a5
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-01-14 14:06:26 +0100

    fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var thinclientRouter = require('./routes/thinclient');
 var thinstationHostsRouter = require('./routes/thinstation.hosts');
 
 var app = express();
-mongoose.connect('mongodb://10.82.24.21/thinclient', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/thinclient', { useNewUrlParser: true });
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
