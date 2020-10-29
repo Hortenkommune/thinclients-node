89364f19ec56865f141f8b545cb6e4df37fb5ddf
Jonas Forsbakk <jonas@forsbakk.priv.no>
2020-10-29 19:41:43 +0100

    added cfg dyn from db

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,20 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: 'ThinClient(s)', clients: clients, groups: groups });
 });
 
+router.get("/thinstation.conf.group-:name", async (request, response) => {
+  try {
+      var group = await GroupModel.findOne({ name: request.params.name }).exec();
+      var output = 'SCREEN_RESOLUTION="' + group.resolution + '"\nXRANDR_OPTIONS="dualscreen"';
+      var buf = Buffer.from(output, 'binary');
+      response.setHeader('Content-type', 'application/octet-stream');
+      response.setHeader('Accept-Ranges', 'bytes');
+      response.setHeader('Content-Length', buf.length);
+      response.setHeader('Cache-Control', 'public, max-age=0');
+      response.write(buf, 'binary');
+      response.end(null, 'binary');
+  } catch (error) {
+      response.status(500).send(error);
+  }
+});
+
 module.exports = router;
